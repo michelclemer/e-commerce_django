2aa157386843a97a71fa7ae6de1236fc14891621
michelclemer <michelclemermonteirodeolino@gmail.com>
2022-03-04 10:57:34 -0300

    setup docker

--- pystore/settings.py
+++ pystore/settings.py
@@ -73,10 +73,15 @@ WSGI_APPLICATION = "pystore.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "",
+        "USER": "",
+        "PASSWORD": "",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
